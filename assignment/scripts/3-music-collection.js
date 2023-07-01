de32d271b85794c15b6479e1e2423597e8cc228b
Jeffrey Varughese <jeffvarughese@gmail.com>
2023-07-01 13:17:37 -0500

    completed addToCollection function and added to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,22 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+    let albumToAdd = {
+        albumTitle: title,
+        artistName: artist,
+        yearPublished: yearPublished,
+    };
+    collection.push(albumToAdd);
+    console.log(albumToAdd);
+}
+
+addToCollection('Californication', 'Red Hot Chili Peppers', 1999);
+addToCollection('Room for Squares', 'John Mayer', 2001);
+addToCollection('Old Church Basement', 'Maverick City Music', 2021);
+addToCollection('The Black Album', 'Jay-Z', 2003);
+addToCollection('XX', 'The XX', 2009);
+addToCollection('Rehad', 'Lecrae', 2010);
+
+console.table(collection);
