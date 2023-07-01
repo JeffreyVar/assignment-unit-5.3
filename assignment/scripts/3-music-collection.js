411261e2b4d864e7355eb865f2786bd86e98b4a3
Jeffrey Varughese <jeffvarughese@gmail.com>
2023-07-01 13:34:36 -0500

    created and tested the showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -18,5 +18,13 @@ addToCollection('Old Church Basement', 'Maverick City Music', 2021);
 addToCollection('The Black Album', 'Jay-Z', 2003);
 addToCollection('XX', 'The XX', 2009);
 addToCollection('Rehad', 'Lecrae', 2010);
-
+console.log(collection);
 console.table(collection);
+
+function showCollection(array) {
+    console.log(array.length);
+    for (item of collection) {
+        console.log(`${item.albumTitle} by ${item.artistName}, published in ${item.yearPublished}`);
+    }
+}
+showCollection(collection);
